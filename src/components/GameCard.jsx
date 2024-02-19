d2da01e983fa6de7277b9cd21c769889443e4a1b
jean <jeandeveweb@gmail.com>
2024-02-19 16:28:27 +0100

    show loading skeletons

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -3,11 +3,12 @@ import React from "react";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 const GameCard = ({ game }) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+    <Card borderRadius={10} overflow="hidden" w='300px'>
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name} </Heading>
               <HStack
--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,9 +1,12 @@
 import useGames from "../hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
-const GameGrid = () => {
-  const { error, games } = useGames();
+import GameCardSkeleton from "./GameCardSkeleton";
 
+const GameGrid = () => {
+  const { error, games, isLoading } = useGames();
+
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -15,9 +18,11 @@ const GameGrid = () => {
           lg: 3,
           xl: 5,
         }}
-        padding='10px'
+        padding="10px"
         spacing={10}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard game={game} key={game.id} />
         ))}
